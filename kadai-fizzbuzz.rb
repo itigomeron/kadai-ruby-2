4ec868409f30ca872bce6a7cc7c0520405b1e3d5
Ubuntu <ubuntu@ip-172-31-30-53.ap-northeast-1.compute.internal>
2022-02-01 05:38:21 +0000

    add kadai-fizzbuzz.rb

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,16 @@
+def fizzbuzz(number)
+if number % 5 == 0 && number % 3 == 0
+    return "FizzBuzz"
+elsif number % 3 == 0
+    return "Fizz"
+elsif number % 5 == 0
+    return "Buzz"
+else 
+    return number
+end
+end
+num_max = 100
+(1..num_max).each do |number|
+    puts fizzbuzz(number)
+end
+
